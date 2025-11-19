6d4e51ba686561226b88afc3922601663eb41049
senna <joaopedrorochasenna@gmail.com>
2025-11-18 23:34:29 -0300

    Alteração de nomes correção

--- src/main/java/br/edu/avaliacao/repositorys/DisciplinaRepository.java
+++ src/main/java/br/edu/avaliacao/repositorys/DisciplinaRepository.java
@@ -1,4 +1,4 @@
-package br.edu.avaliacao.repository;
+package br.edu.avaliacao.repositorys;
 
 import br.edu.avaliacao.models.Disciplina;
 import jakarta.persistence.*;
--- src/main/java/br/edu/avaliacao/repositorys/ProcessoAvaliativoRepository.java
+++ src/main/java/br/edu/avaliacao/repositorys/ProcessoAvaliativoRepository.java
@@ -1,4 +1,4 @@
-package br.edu.avaliacao.repository;
+package br.edu.avaliacao.repositorys;
 
 import br.edu.avaliacao.models.ProcessoAvaliativo;
 import jakarta.persistence.*;
